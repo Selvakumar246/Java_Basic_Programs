cbfdd3f4d35d107a6a10bd0226ef8ddfab673004
Selvakumar246 <vjselvakumarofficial@gmail.com>
2025-09-19 21:15:21 +0530

    created Simple_Interest.java

--- Simple_Interest.java
+++ Simple_Interest.java
@@ -1,3 +1,17 @@
+//question 3: find simple interest.
+import java.util.Scanner;
 public class Simple_Interest {
-    
+        public static void main(String[] args) {
+            Scanner input=new Scanner(System.in);
+            System.out.print("enter principle amount (in ₹):");
+            float p=input.nextFloat();
+            System.out.print("enter time period(yrs):");
+            int t= input.nextInt();
+            System.out.print("enter Rate of interest (in %):");
+            float r= input.nextFloat();
+
+            float si=(p*t*r)/100;
+            System.out.println("simple interest Amount is: "+si);
+
+        }
 }
